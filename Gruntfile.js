60465b1303ad243c524131e6baa6859f6b8814be
tcmorin <tcmorin@gmail.com>
2014-05-22 22:08:26 +0200

    review build files and then first snapshot

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,16 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            default: {
+                options:{
+                    port: 3001,
+                    base: '',
+                    keepalive: true
+                }
+            }
+        },
         karma: {
             default: {
                 configFile: 'karma.conf.js'
@@ -12,10 +22,45 @@ module.exports = function (grunt) {
                     src: ['src/**/*.js', 'spec/**/*.js']
                 }
             }
+        },
+        uglify: {
+            default: {
+                options: {
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+                    sourceMap: true,
+                    sourceMapIncludeSources: true,
+                    sourceMapName: 'dist/cqrs.map'
+                },
+                files: {
+                    'dist/cqrs.min.js': ['src/cqrs.js']
+                }
+            }
+        },
+        bumpup: ['bower.json', 'package.json'],
+        tagrelease: {
+            file: 'package.json',
+            prefix: '',
+            commit: true
+        },
+        exec: {
+            'update_master':{
+                cmd: 'git push origin master --tags'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-tagrelease');
+    grunt.loadNpmTasks('grunt-bumpup');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('build', ['jshint', 'karma', 'uglify']);
+    grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
+
+    grunt.registerTask('push', ['exec:update_master']);
+    grunt.registerTask('bump-push', ['bump:patch','push']);
 
 };
--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // Start these browsers, currently available:
@@ -52,7 +52,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
+    singleRun: true,
 
 
      plugins: [
